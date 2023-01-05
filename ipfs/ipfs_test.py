0d93070b111892510127a9c2d9f2ec646794c876
Silvano <silvanoross3@gmail.com>
2023-01-04 19:50:01 -0800

    created pinata helper functions and a simple app

--- ipfs/ipfs_test.py
+++ ipfs/ipfs_test.py
@@ -1,67 +1,70 @@
-# import required libraries
-
+# Main file for pinning files to ipfs system and generating metadata
 import os
 import json
-import requests
-from pathlib import Path
-import streamlit as st
-import ipfshttpclient
-from dotenv import load_dotenv
 from web3 import Web3
-from pinata import Pinata
-# functions for pinata
-
+from pathlib import Path
+from dotenv import load_dotenv
+import streamlit as st
+# personalized functions for api usage
+from pinata_helper import pin_file_to_ipfs, pin_json_to_ipfs, convert_data_to_json
 
 # load environment variables
 load_dotenv()
 
 # Define and connect a new Web3 provider
-w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
+w3 = Web3(Web3.HTTPProvider(os.getenv("WEB_PROVIDER_URI")))
 
-# Create pinata env variables
-api_key = os.getenv("PINATA_API_KEY")
-secret_key = os.getenv("PINATA_SECRET_KEY")
-access_token = os.getenv("JWT")
+# Define function for generating pinata pin
+def pin_file(file_name, associated_account, desired_file):
+    # Pin the file to IPFS with Pinata
+    ipfs_file_hash = pin_file_to_ipfs(desired_file.getvalue())
 
-# Load pinata client
-pinata = Pinata(api_key=api_key, secret_key=secret_key, access_token=access_token)
+    # Build a token metadata file for the artwork
+    token_json = {
+        "name": file_name,
+        "file": ipfs_file_hash,
+        "associated_account": associated_account
+    }
+    json_data = convert_data_to_json(token_json)
 
-# # pinata api url
-# pinata_url = "https://api.pinata.cloud"
+    # Pin the json to IPFS with Pinata
+    json_ipfs_hash = pin_json_to_ipfs(json_data)
+
+    return json_ipfs_hash
+
+#################################################################################
+#------------------------------ Streamlit app ----------------------------------#
+#################################################################################
+
+st.title("IPFS File Uploader")
+st.write("Choose an account to get started")
+
+# account that will be associated with file upload
+accounts = w3.eth.accounts
+address = st.selectbox("Select Account Associated with File", options=accounts)
+st.markdown("---")
+
+# file name 
+file_name = st.text_input("Enter the File Name: ")
 
 
-# import requests
-# url = "https://api.pinata.cloud/data/testAuthentication"
-# payload={} 
-# headers = {'Authorization': f'Bearer {access_token}'}
-# response = requests.request("GET", url, headers=headers, data=payload)
-# print(response.text)
+# file uploader that allows many different kinds of files
+file = st.file_uploader("Choose File to Mint", type=[
+    "jpeg", "jpg", "png", "pdf", "gif", "txt", "docx", "ppt", "csv", "mp3", "mp4", "wav", "xlsx"
+    ])
 
-def pin_to_pinata(file_name, identity, file_path):
-    url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
-    payload={'pinataOptions': "{'cidVersion': 1}",
-             
-             'pinataMetadata': "{'name': f"{file_name}",
-             
-             'keyvalues': "{'id': f'{identity}'}"
-             
-             }
-             
-    files=[
-        ('file',(file_name, open(file_path,'rb'), 'application/octet-stream'))
-           ]
-    headers = {
-        'Authorization': f'Bearer {access_token}'
-               }
-    response = requests.request("POST", url, headers=headers, data=payload, files=files)
-    print(response.text)
+if st.button("Mint and Receive a Reward"):
+    artwork_ipfs_hash = pin_file(file_name=file_name, desired_file=file, associated_account=address, )
+    artwork_uri = f"ipfs://{artwork_ipfs_hash}"
+    
+    
+    st.write("File Minted")
+    st.write("You can view the pinned metadata file with the following IPFS Gateway Link")
+    st.markdown(f"[File IPFS Gateway Link](https://ipfs.io/ipfs/{artwork_ipfs_hash})")
+    st.balloons
 
-# Stramlit app #
 
-# st.markdown("# Pinata IPFS test")
 
-# # Upload Box
-# data = st.file_uploader("Upload Artwork Here")
 
-# Create Register Button
-# if st.button("Register NFT"):
+
+
